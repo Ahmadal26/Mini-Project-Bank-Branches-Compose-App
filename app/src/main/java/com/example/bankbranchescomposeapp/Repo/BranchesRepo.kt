eda8e81ac56fa61c7b7fe5a7473bb3933a1416d8
Nada B E J M A Alshaibani <83315@kfh.com>
2024-01-25 08:30:48 +0300

    Card Layout and Favourite Button

--- app/src/main/java/com/example/bankbranchescomposeapp/Repo/BranchesRepo.kt
+++ app/src/main/java/com/example/bankbranchescomposeapp/Repo/BranchesRepo.kt
@@ -6,7 +6,39 @@ import com.example.bankbranchescomposeapp.Data.BranchType
 class BranchesRepo {
     companion object {
         var dummyBranchesList = listOf(
-            BranchData(1,"sal","yugd","678944","1-5","gggh",9,BranchType.ONLINEBANCH)
+            BranchData(
+                1, "Sharq",
+                "Sharq", "678944",
+                "7 AM - 3 PM",
+                "gggh", imageUri = 8,
+                BranchType.ONSITEBRANCH
+            ),
+
+            BranchData(
+                3, "Khaldiya",
+                "Khaldiya", "253889",
+                "7 AM - 3 PM",
+                "gggh", imageUri = 9,
+                BranchType.ONLINEBANCH
+            ),
+
+            BranchData(
+                2, "Kaifan",
+                "Kaifan", "365432",
+                "7 AM - 3 PM",
+                "gggh", imageUri = 6,
+                BranchType.ONLINEBANCH
+            ),
+
+            BranchData(
+                9, "Shamiya",
+                "Shamiya", "678944",
+                "7 AM - 3 PM",
+                "gggh", imageUri = 10,
+                BranchType.ONSITEBRANCH
+            )
+
         )
+
     }
 }
